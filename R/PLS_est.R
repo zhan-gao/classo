9fc0b28ad8940943067c70bfba03cc05ad9584fb
zhangao <zhangao@usc.edu>
2019-08-02 11:19:34 -0700

    1/ minor improve in init_est function. 2/ skip the DCP check in CVXR function. 3/ remove dependency on Rmosek. Change default solver to CVXR.

--- R/PLS_est.R
+++ R/PLS_est.R
@@ -22,8 +22,8 @@
 #'
 #'
 
-PLS.mosek <- function(N, TT, y, X, K, lambda, beta0 = NULL, R = 500, tol = 1e-04,
-    post_est = TRUE, bias_corr = FALSE) {
+PLS.mosek <- function(N, TT, y, X, K, lambda, beta0 = NULL, R = 500,
+    tol = 1e-04, post_est = TRUE, bias_corr = FALSE) {
 
     p <- dim(X)[2]
 
@@ -96,7 +96,6 @@ PLS.mosek <- function(N, TT, y, X, K, lambda, beta0 = NULL, R = 500, tol = 1e-04
 #' PLS estimation by the iterative algorithm via CVXR
 #'
 #' @inheritParams PLS.mosek
-#' @param solver 'ECOS' or 'MOSEK'
 #'
 #' @return A list contains estimated coeffcients and group struncture
 #' \item{b.est}{N * p matrix containing estimated slope for each individual}
@@ -108,7 +107,7 @@ PLS.mosek <- function(N, TT, y, X, K, lambda, beta0 = NULL, R = 500, tol = 1e-04
 #'
 
 PLS.cvxr <- function(N, TT, y, X, K, lambda, beta0 = NULL, R = 500, tol = 1e-04,
-    solver = "ECOS", post_est = TRUE, bias_corr = FALSE) {
+                     post_est = TRUE, bias_corr = FALSE) {
 
     p <- dim(X)[2]
 
@@ -151,18 +150,32 @@ PLS.cvxr <- function(N, TT, y, X, K, lambda, beta0 = NULL, R = 500, tol = 1e-04,
 
             XX = bdiag(X.list)
 
-            ## Original commented out obj = Minimize( sum_squares(y - XX %*% b)/(N *
-            ## TT) + obj*(lambda/N) ) End Original commented out
+            ## Original commented out obj = Minimize( sum_squares(y - XX %*% b)/(N
+            ## * TT) + obj*(lambda/N) ) End Original commented out
 
             ## Code added and modified
-            obj = Minimize(sum_squares(y - XX %*% vec(b))/(N * TT) + obj1 *
-                (lambda/N))
+            obj = Minimize(sum_squares(y - XX %*% vec(b))/(N * TT) +
+                obj1 * (lambda/N))
             ## End Code added and modified
             Prob = Problem(obj)
-            cvxr.out = solve(Prob, solver = solver)
 
-            a.out[k, ] = cvxr.out$getValue(a)
-            b.out[, , k] = matrix(cvxr.out$getValue(b), N, p, byrow = TRUE)
+
+            prob_data <- get_problem_data(Prob, solver = "ECOS")
+            solver_output <- ECOSolveR::ECOS_csolve(c = prob_data[["c"]],
+                                                    G = prob_data[["G"]],
+                                                    h = prob_data[["h"]],
+                                                    dims = prob_data[["dims"]],
+                                                    A = prob_data[["A"]],
+                                                    b = prob_data[["b"]])
+            direct_soln <- unpack_results(Prob, "ECOS", solver_output)
+
+            # cvxr.out = solve(Prob, solver = solver)
+
+            # a.out[k, ] = cvxr.out$getValue(a)
+            # b.out[, , k] = matrix(cvxr.out$getValue(b), N, p, byrow = TRUE)
+
+            a.out[k, ] = direct_soln$getValue(a)
+            b.out[, , k] = matrix(direct_soln$getValue(b), N, p, byrow = TRUE)
 
         }
 
@@ -208,6 +221,159 @@ PLS.cvxr <- function(N, TT, y, X, K, lambda, beta0 = NULL, R = 500, tol = 1e-04,
     return(result)
 }
 
+#' PLS estimation by the iterative algorithm via NLOPTR
+#'
+#' @inheritParams PLS.mosek
+#' @param algo choose algorithm for nloptr
+#'
+#' @return A list contains estimated coeffcients and group struncture
+#' \item{b.est}{N * p matrix containing estimated slope for each individual}
+#' \item{a.out}{K * p matrix containing estimated slope for each group}
+#' \item{group.est}{group_id for each individual}
+#' \item{converge}{A boolean indicating whether convergence criteria is met }
+#'
+#' @export
+#'
+
+PLS.nlopt <- function(N, TT, y, X, K, lambda, beta0 = NULL, R = 500,
+    tol = 1e-04, post_est = TRUE, bias_corr = FALSE, algo = "NLOPT_LN_NELDERMEAD") {
+
+
+    p <- dim(X)[2]
+
+    if (is.null(beta0)) {
+        # Use individual regression result as the initial value
+        beta0 = numeric()
+        for (i in 1:N) {
+            ind <- ((i - 1) * TT + 1):(i * TT)
+            yy <- y[ind, ]
+            XX <- X[ind, ]
+            beta0 <- c(beta0, solve(t(XX) %*% XX) %*% (t(XX) %*% yy))
+        }
+    }
+
+    init <- c(beta0, c(1, 1))
+
+    b.out <- array(beta0, c(N, p, K))
+    a.out <- matrix(0, K, p)
+
+    b.old <- matrix(1, N, p)
+    a.old <- matrix(1, 1, p)
+
+    for (r in 1:R) {
+
+        for (k in 1:K) {
+
+            # N * 1: consider it as gamma
+            penalty.out <- pen.generate(b.out, a.out, N, p, K, k)
+
+            # opt.out <- optimx(fn = obj, gr = obj.grad, par = init, penalty =
+            # penalty.out);
+
+            # opts = list(algorithm = 'NLOPT_LN_NELDERMEAD', xtol_rel = 1e-5,
+            # maxeval = 500);
+            opts = list(algorithm = algo, xtol_rel = 1e-05, maxeval = 2500)
+            nlopt.out <- nloptr(x0 = init, eval_f = obj, eval_grad_f = obj.grad,
+                opts = opts, N = N, penalty = penalty.out)
+            # eval_grad_f = obj.grad,
+            coef.temp <- nlopt.out$solution
+
+            a.out[k, ] <- coef.temp[(p * N + 1):(p * (N + 1))]
+            b.out[, , k] <- matrix(coef.temp[1:(N * p)], N, p, byrow = TRUE)
+
+
+        }
+
+        # Check the convergence criterion
+        a.new <- a.out[K, ]
+        b.new <- b.out[, , K]
+
+        if (criterion(a.old, a.new, b.old, b.new, tol)) {
+            break
+        }
+        # Update
+        a.old <- a.out[K, ]
+        b.old <- b.out[, , K]
+    }
+
+    # put b.out to nearest a.out and get the group estimation
+
+    a.out.exp <- aperm(array(a.out, c(K, p, N)), c(3, 2, 1))
+    d.temp <- (b.out - a.out.exp)^2
+    dist <- sqrt(apply(d.temp, c(1, 3), sum))
+    group.est <- apply(dist, 1, which.min)
+
+
+    # Post estimation
+    if (post_est) {
+        if (bias_corr) {
+            a.out <- post.corr(group.est, a.out, y, X, K, p, N, TT)
+        } else {
+            a.out <- post.lasso(group.est, a.out, y, X, K, p, N, TT)
+        }
+    }
+
+
+    b.est <- matrix(999, N, p)
+    for (i in 1:N) {
+        group <- group.est[i]
+        b.est[i, ] <- a.out[group, ]
+    }
+
+    result <- list(b.est = b.est, a.out = a.out, group.est = group.est,
+        converge = (r < R))
+
+    return(result)
+
+}
+
+
+##########################################################
+obj <- function(coeff, N = NULL, penalty = NULL) {
+
+    # objective function
+
+    B <- matrix(coeff[1:(p * N)], nrow = p)
+    ee <- X %*% B
+
+    ii <- as.logical(c(rep(c(rep(1, TT), rep(0, TT * N)), N - 1), rep(1,
+        TT)))
+    ob <- (sum((y - ee[ii])^2))/(N * TT)
+
+    ob <- ob + sum(sqrt(apply(matrix((coeff[1:(p * N)] - rep(coeff[(p *
+        N + 1):(p * (N + 1))], N))^2, nrow = N, byrow = TRUE), 1, sum)) *
+        penalty * (lambda/N))
+    return(ob)
+
+}
+##########################################################
+
+obj.grad <- function(coeff, N = NULL, penalty = NULL) {
+
+    # gradient of the objective function
+
+    B <- matrix(coeff[1:(p * N)], nrow = p)
+    ee <- X %*% B
+    ii <- as.logical(c(rep(c(rep(1, TT), rep(0, TT * N)), N - 1), rep(1,
+        TT)))
+    ob.grad.1 <- as.vector(t(apply(array(matrix(sum((y - ee[ii])^2),
+        TT * N, p) * X, c(TT, N, p)), c(2, 3), sum))) * 2/(N * TT)
+
+    b.minus.a <- coeff[1:(p * N)] - rep(coeff[(p * N + 1):(p * (N + 1))],
+        N)
+    norm2 <- sqrt(apply(matrix((b.minus.a)^2, nrow = N, byrow = TRUE),
+        1, sum))
+    ob.grad.1 <- ob.grad.1 + (b.minus.a/rep(norm2, rep(p, N))) * penalty *
+        (lambda/N)
+
+    ob.grad.2 <- apply(matrix((-b.minus.a/rep(norm2, rep(p, N))) * penalty *
+        (lambda/N), nrow = N, byrow = TRUE), 2, sum)
+
+    return(c(ob.grad.1, ob.grad.2))
+
+}
+
+
 
 ##########################################################
 pen.generate <- function(b, a, N, p, K, kk) {
@@ -232,8 +398,8 @@ opt.mosek <- function(y, X, penalty, N, TT, K, p, lambda) {
     # call Mosek solver to solve the optimization conic programming
 
 
-    # INPUT Arg: dimensions N, TT, K, p tuning parameter lambda data y(TN *
-    # 1), X(TN * P) parameter penalty (N*1) numeric
+    # INPUT Arg: dimensions N, TT, K, p tuning parameter lambda data y(TN
+    # * 1), X(TN * P) parameter penalty (N*1) numeric
 
 
     # set sense of optim and tolerance
@@ -244,8 +410,8 @@ opt.mosek <- function(y, X, penalty, N, TT, K, p, lambda) {
     # nu_i (i=1,2,...,N) , mu_i (i=1,2,...,N) alpha_k, s_i (i=1,2,...,N),
     # r_i (i=1,2,...,N), t_i (i=1,2,...,N), w_i (i=1,2,...,N)
 
-    prob$c <- c(rep(0, N * (2 * p + TT + 2) + p), rep(1/(N * TT), N), penalty *
-        lambda/N)
+    prob$c <- c(rep(0, N * (2 * p + TT + 2) + p), rep(1/(N * TT), N),
+        penalty * lambda/N)
 
     # lieanr constraint: matrix A
 
@@ -257,25 +423,25 @@ opt.mosek <- function(y, X, penalty, N, TT, K, p, lambda) {
         X.split[[i]] <- X[ind, ]
     }
 
-    A.y <- cbind(bdiag(X.split), Diagonal(TT * N), Matrix(0, TT * N, N *
-        (p + 4) + p))
+    A.y <- cbind(bdiag(X.split), Diagonal(TT * N), Matrix(0, TT * N,
+        N * (p + 4) + p))
 
     A.0 <- cbind(Diagonal(N * p), Matrix(0, N * p, TT * N), -Diagonal(N *
         p), -matrix(diag(p), N * p, p, byrow = TRUE), Matrix(0, N * p,
         N * 4))
 
-    A.nhalf <- cbind(Matrix(0, N, N * (2 * p + TT) + p), Diagonal(N), Matrix(0,
-        N, N), -Diagonal(N)/2, Matrix(0, N, N))
+    A.nhalf <- cbind(Matrix(0, N, N * (2 * p + TT) + p), Diagonal(N),
+        Matrix(0, N, N), -Diagonal(N)/2, Matrix(0, N, N))
 
-    A.phalf <- cbind(Matrix(0, N, N * (2 * p + TT) + p), Matrix(0, N, N),
-        Diagonal(N), -Diagonal(N)/2, Matrix(0, N, N))
+    A.phalf <- cbind(Matrix(0, N, N * (2 * p + TT) + p), Matrix(0, N,
+        N), Diagonal(N), -Diagonal(N)/2, Matrix(0, N, N))
 
     A <- rbind(A.y, A.0, A.nhalf, A.phalf)
     prob$A <- as(A, "CsparseMatrix")
 
     # linear constraint: upperbound and lowerbound
-    prob$bc <- rbind(blc = c(y, rep(0, N * p), rep(-1/2, N), rep(1/2, N)),
-        buc = c(y, rep(0, N * p), rep(-1/2, N), rep(1/2, N)))
+    prob$bc <- rbind(blc = c(y, rep(0, N * p), rep(-1/2, N), rep(1/2,
+        N)), buc = c(y, rep(0, N * p), rep(-1/2, N), rep(1/2, N)))
     # t_i \geq 0
     prob$bx <- rbind(blx = c(rep(-Inf, N * (2 * p + TT + 2) + p), rep(0,
         N), rep(-Inf, N)), bux = c(rep(Inf, N * (2 * p + TT + 4) + p)))
@@ -289,9 +455,10 @@ opt.mosek <- function(y, X, penalty, N, TT, K, p, lambda) {
         r.i <- bench + N + i
         nu.i <- (N * p + (i - 1) * TT + 1):(N * p + i * TT)
         w.i <- bench + 3 * N + i
-        mu.i <- (N * (TT + p) + (i - 1) * p + 1):(N * (TT + p) + i * p)
-        CC <- cbind(CC, list("QUAD", c(r.i, nu.i, s.i)), list("QUAD", c(w.i,
-            mu.i)))
+        mu.i <- (N * (TT + p) + (i - 1) * p + 1):(N * (TT + p) + i *
+            p)
+        CC <- cbind(CC, list("QUAD", c(r.i, nu.i, s.i)), list("QUAD",
+            c(w.i, mu.i)))
     }
     prob$cones <- CC
     rownames(prob$cones) <- c("type", "sub")
--- R/init_est.R
+++ R/init_est.R
@@ -10,9 +10,11 @@ init_est <- function(X, y, TT){
     beta0 <- matrix(0, N, p);
     for(i in 1:N){
         ind <- ( (i-1)*TT+1 ):(i*TT);
-        yy <- y[ind, ]
+        yy <- y[ind]
         XX <- X[ind, ]
-        beta0[i, ] <- solve( t(XX) %*% XX ) %*% ( t(XX) %*% yy );
+
+        # beta0[i, ] <- solve( t(XX) %*% XX ) %*% ( t(XX) %*% yy )
+        beta0[i, ] <- lsfit(x = XX, y = yy, intercept = FALSE)$coefficients
     }
 
     return(beta0)

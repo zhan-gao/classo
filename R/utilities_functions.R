e2ed7182fc755aa4d6cc46652f6c492b52b03f1e
zhangao <zhangao@usc.edu>
2019-08-02 23:40:28 -0700

    export group coerce

--- R/utilities_functions.R
+++ R/utilities_functions.R
@@ -1,5 +1,6 @@
 #' @export data.normalization
 #' @export demean
+#' @export group.coerce
 
 demean <- function(yy, N, TT){
 
@@ -74,3 +75,33 @@ data.normalization <- function(yy, N, TT){
 
     return(list(y = y, y.raw = y.raw))
 }
+
+
+group.coerce <- function(group.est, a.out, group0, a0, N, N.frac, K, p){
+
+    # auxiliary function useful in simulation experiments
+
+    a.est <- matrix(0,K,p);
+    group <- rep(0,N);
+
+    for(k in 1:K){
+        dif <- apply( (matrix( a.out[k,], K, p, byrow = TRUE) - a0)^2, 1, sum);
+        gg <- which.min(dif);
+
+        a.est[gg, ] <- a.out[k, ];
+        group[group.est == k] <- gg;
+    }
+
+    if( sum(group == 0) ){
+        noquote("Error: some inidividuals are not classfied!")
+    }
+
+    # correct ratio
+    ratio <- sum( group == group0 ) / N;
+    # rmse
+    weighted.se <- sum( ( a.est[,1] - a0[,1] )^2 * N.frac );
+
+    return( list(ratio = ratio, se = weighted.se,
+                 a = a.est, group = group) );
+}
+
